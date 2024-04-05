2b63edbffc09cb7ed5981999f3410d23c434aeca
daikoku-nakamura <k-nakamura@daikoku-com.jp>
2024-04-05 13:25:19 +0900

    metadetaでnoindexを設定しました

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ import './globals.css';
 export const metadata: Metadata = {
   title: 'Daikoku Blog.',
   description: 'This is Daikoku Blog.',
+  robots: {
+    index: false,
+    follow: false,
+    googleBot: {
+      index: false,
+      follow: false,
+    },
+  },
 };
 
 export default function RootLayout({

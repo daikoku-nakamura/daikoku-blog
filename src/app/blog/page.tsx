91ea51ab5c594ef30895c4d352cfcf4dbf9b66eb
knm22mukk <knm22mukk@gmail.com>
2024-04-28 15:37:58 +0900

    microcms-js-sdkを導入して記事一覧の表示まで作成しました

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,13 +1,15 @@
 import BlogCard from '@/components/BlogCard';
 import Breadcrumb from '@/components/Breadcrumb';
 import SectionHeader from '@/components/SectionHeader';
+import { getBlogList } from '@/libs/microcms';
 
-export default function Blogs() {
+export default async function Blogs() {
+  const blogData = await getBlogList();
   return (
     <div>
       <Breadcrumb lists={[{ name: 'ブログ一覧' }]} />
       <SectionHeader subHeading='blogs' title='ブログ記事一覧' />
-      <BlogCard />
+      <BlogCard blogs={blogData.contents} />
     </div>
   );
 }

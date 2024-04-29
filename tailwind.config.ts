697cb79684ae94cf3a41f497b2a5054408017a35
knm22mukk <knm22mukk@gmail.com>
2024-04-29 11:37:56 +0900

    ブログの詳細ページを作成しました

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,11 +1,29 @@
 import type { Config } from 'tailwindcss';
+import type { PluginAPI } from 'tailwindcss/types/config';
 
 const config: Config = {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      typography: (theme: PluginAPI['theme']) => ({
+        DEFAULT: {
+          css: {
+            maxWidth: false,
+            h2: {
+              background: theme('colors.green.600'),
+              padding: theme('spacing.4'),
+              color: theme('colors.white'),
+            },
+            h3: {
+              borderLeft: "8px solid theme('colors.green.600')",
+              padding: theme('spaciing.8'),
+            },
+          },
+        },
+      }),
+    },
   },
-  plugins: [],
+  plugins: [require('@tailwindcss/typography')],
 };
 export default config;

1591cf120d157335ce660d7de45b8f7879fce1e4
daikoku-nakamura <k-nakamura@daikoku-com.jp>
2024-05-24 17:20:48 +0900

    商品一覧ページの画像の下に商品名を表示するように変更しました。

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -8,14 +8,16 @@ export default function ProductList() {
       <div className='flex flex-col justify-center'>
         <div className='grid grid-cols-1 gap-4 lg:grid-cols-4 sm:grid-cols-2'>
           {productImages.map(product => (
-            <Image
-              key={product.src}
-              src={product.src}
-              width={300}
-              height={300}
-              alt={product.alt}
-              className='object-cover w-full bg-gray-500 aspect-square'
-            />
+            <div className='' key={product.src}>
+              <Image
+                src={product.src}
+                width={300}
+                height={300}
+                alt={product.alt}
+                className='object-cover w-full aspect-square'
+              />
+              <p className='text-center mt-2 text-gray-600'>{product.alt}</p>
+            </div>
           ))}
         </div>
       </div>
